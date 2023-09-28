3370f533dad2c20fb7bea1410af852ec32f038da
Natalia Bandurova <natacsp@mail.ru>
2023-09-28 15:16:13 +0300

    feat: add image gallery layout

---
+++ image-galery/index.js
@@ -0,0 +1,16 @@
+
+const url = 'https://type.fit/api/quotes';
+
+https://api.unsplash.com/photos/random?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo 
+
+async function getData() {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  }
+  getData();
+
+
+  data.urls.regular;
+
+  ?qwery=image&per_page=16
