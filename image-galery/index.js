e61cfbb265f3fe61546c071ee56f3ca85d0ed081
Natalia Bandurova <natacsp@mail.ru>
2023-09-28 17:24:45 +0300

    feat: add js script

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,16 +1,66 @@
 
-const url = 'https://type.fit/api/quotes';
+const apiKey = 'RQVsKWs76wW1DBXgxzf5UrxIdmZ_Zj96y8C2IHrPMwI';
 
-https://api.unsplash.com/photos/random?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo 
+const imageContainer = document.querySelector('.image-container');
+const searchInput = document.getElementById('search');
+const searchButton = document.querySelector('.searchButton');
+const clearSearch = document.querySelector('.clearSearch');
 
-async function getData() {
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-  }
-  getData();
+const searchImages = () => {
+    const query = searchInput.value.trim();
+    if (query !== '') {
+        fetchImages(query);
+    }
+}
+
+const fetchImages = async (query) => {
+    const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=20&client_id=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Error loading images');
+        }
+
+        const data = await response.json();
+        displayImages(data.results);
+    } catch (error) {
+        console.error('Error loading images:', error);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    fetchImages('nature');
+    searchInput.focus();
+});
+
+searchInput.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') {
+        searchImages();
+    }
+})
+
+searchButton.addEventListener('click', searchImages);
 
 
-  data.urls.regular;
+//-----------------default clear search--------------------
+// clearSearch.addEventListener('click', )
 
-  ?qwery=image&per_page=16
+
+const displayImages = (images) => {
+    imageContainer.innerHTML = '';
+
+    images.forEach((image) => {
+        const imageDiv = document.createElement('div');
+        imageDiv.classList.add('image');
+        const img = document.createElement('img');
+        img.src = image.urls.small;
+        img.alt = image.alt_description;
+        imageDiv.appendChild(img);
+        imageContainer.appendChild(imageDiv);
+    });
+}
+
+
+
+
